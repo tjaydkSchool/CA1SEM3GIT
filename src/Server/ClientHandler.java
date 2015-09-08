747f4dd3fa997eb55733ab81e287952d06ddecaa
tjaydkSchool <dennis.michael@jubii.dk>
2015-09-08 13:36:29 +0200

    MSNServer, ClientHandler, Utility v 1.0 and ExceptionClass
    Created MSN Server v. 1.0
    Created ClientHandler v. 1.0:
    handles the protocol with switch
    Created Utility Class v. 1.0
    Created ExceptionClass:
    able to handle ClientName... exception

---
+++ src/Server/ClientHandler.java
@@ -0,0 +1,72 @@
+package Server;
+
+import Exceptions.ClientNameAlreadyInUseException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ClientHandler extends Thread {
+
+    private Scanner input;
+    private PrintWriter output;
+    private Socket socket;
+    private MSNServer server;
+
+    public ClientHandler(Socket socket, MSNServer server) throws IOException {
+        input = new Scanner(socket.getInputStream());
+        output = new PrintWriter(socket.getOutputStream(), true);
+        this.socket = socket;
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        Boolean closeConnection = false;
+
+        do {
+            String msgInput = input.next();
+            List<String> recieverList = new ArrayList();
+
+            Scanner split = new Scanner(msgInput).useDelimiter("#");
+            String action = split.next();
+            String msg;
+
+            switch (action) {
+                case "STOP":
+                    closeConnection = true;
+                    break;
+                case "MSG":
+                    String recievers = split.next();
+                    Scanner listRecievers = new Scanner(recievers).useDelimiter(",");
+                    while (listRecievers.hasNext()) {
+                        recieverList.add(listRecievers.next());
+                    }
+                    msg = split.next();
+                    System.out.println("Message from: " + this.getName() + ": " + msg);
+                    break;
+                case "USER":
+                    String clientName = split.next();
+                    List<ClientHandler> cl = server.getClientList();
+                    try {
+                        for (int i = 0; i < cl.size(); i++) {
+                            if (!cl.get(i).equals(clientName)) {
+                                this.setName(clientName);
+                            } else {
+                                throw new ClientNameAlreadyInUseException();
+                            }
+                        }
+                    } catch (ClientNameAlreadyInUseException ex) {
+                        System.out.println("Username already in use");
+                    }
+            }
+        } while (!closeConnection);
+    }
+
+    public void send(String msg) {
+        output.println(msg);
+    }
+
+}
